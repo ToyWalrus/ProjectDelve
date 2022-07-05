827c0bef044b487e0306dec51a8b662316f7dc83
Trevor <tkhowell95@gmail.com>
2022-07-05 11:46:57 -0600

    Wheel updates in editor on resource update

--- scripts/data/wheel_section_data.gd
+++ scripts/data/wheel_section_data.gd
@@ -1,13 +1,47 @@
+tool
 extends Resource
 
 class_name WheelSectionData
 
-export(float, 0, 1) var percent_of_wheel
-export(String) var section_name
-export(int) var attack_points
-export(int) var defense_points
-export(int) var special_points
-export(int, 0, 6) var range_points
-export(bool) var miss
+export(float, 0, 1) var percent_of_wheel setget _set_percent
+export(String) var section_name setget _set_name
+export(int) var attack_points setget _set_atk
+export(int) var defense_points setget _set_def
+export(int) var special_points setget _set_spec
+export(int, 0, 6) var range_points setget _set_range
+export(bool) var miss setget _set_miss
 
-enum WheelSymbol { attack, defense, special, miss, range_2, range_3, range_4, range_5, range_6 }
+
+func _set_percent(val):
+	percent_of_wheel = val
+	emit_changed()
+
+
+func _set_name(val):
+	section_name = val
+	emit_changed()
+
+
+func _set_atk(val):
+	attack_points = val
+	emit_changed()
+
+
+func _set_def(val):
+	defense_points = val
+	emit_changed()
+
+
+func _set_spec(val):
+	special_points = val
+	emit_changed()
+
+
+func _set_range(val):
+	range_points = val
+	emit_changed()
+
+
+func _set_miss(val):
+	miss = val
+	emit_changed()
--- scripts/wheel_section.gd
+++ scripts/wheel_section.gd
@@ -3,7 +3,7 @@ extends Sprite
 
 class_name WheelSection
 
-onready var _icon_container = $SectionName/Icons
+onready var _icon_row_container = $SectionName/IconRows
 onready var _section_name = $SectionName
 
 export(Resource) var wheel_section_data setget _set_data
@@ -14,33 +14,52 @@ export(Texture) var lightning_icon
 export(Texture) var miss_icon
 
 
-func _set_data(section_data):
-	wheel_section_data = section_data
+func _ready():
 	_update_wheel()
 
 
-func _update_wheel():
-	if not wheel_section_data or not has_node("SectionName/Icons"):
-		return
-	if not _icon_container:
-		_icon_container = $SectionName/Icons
+func _set_data(section_data):
+	if wheel_section_data:
+		wheel_section_data.disconnect("changed", self, "_update_wheel")
+	wheel_section_data = section_data
+	if wheel_section_data:
+		wheel_section_data.connect("changed", self, "_update_wheel")
 
-	for child in _icon_container.get_children():
+
+func _update_wheel():
+	if not wheel_section_data or not has_node("SectionName/IconRows"):
+		return
+	if not _icon_row_container:
+		_icon_row_container = $SectionName/IconRows
+
+	for child in _icon_row_container.get_children():
 		child.queue_free()
 
 	if wheel_section_data.miss:
-		_add_icons(1, miss_icon, "Miss")
+		_add_icon_row(1, miss_icon, "Miss")
 	else:
-		_add_icons(wheel_section_data.attack_points, sword_icon, "Sword")
-		_add_icons(wheel_section_data.special_points, lightning_icon, "Lightning")
-		_add_icons(wheel_section_data.defense_points, shield_icon, "Shield")
+		_add_icon_row(wheel_section_data.attack_points, sword_icon, "Attack")
+		_add_icon_row(wheel_section_data.special_points, lightning_icon, "Special")
+		_add_icon_row(wheel_section_data.defense_points, shield_icon, "Defense")
 
 
-func _add_icons(amount, texture, node_prefix = "TexRect"):
+func _add_icon_row(amount, texture, name):
+	if amount <= 0:
+		return
+	var icon_row := HBoxContainer.new()
+	icon_row.name = name
+	icon_row.alignment = BoxContainer.ALIGN_CENTER
+	_icon_row_container.add_child(icon_row)
+	icon_row.owner = get_tree().edited_scene_root
+	_add_icons(icon_row, amount, texture, name)
+
+
+func _add_icons(container, amount, texture, node_prefix = "TexRect"):
 	for i in range(0, amount):
 		var tex_rect := TextureRect.new()
 		tex_rect.texture = texture
 		tex_rect.name = node_prefix + "_" + str(i + 1)
 		tex_rect.stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
-		_icon_container.add_child(tex_rect)
+		container.add_child(tex_rect)
 		tex_rect.owner = get_tree().edited_scene_root
+		tex_rect.rect_scale = Vector2(2, 2)
