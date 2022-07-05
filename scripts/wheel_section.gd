bba6284d0666b729b507c860a46c431e4d405b0b
Trevor <tkhowell95@gmail.com>
2022-07-05 13:31:53 -0600

    Change row to grid for icons; scale icons

--- scripts/wheel_section.gd
+++ scripts/wheel_section.gd
@@ -5,7 +5,7 @@ class_name WheelSection
 
 onready var _pivot = $Pivot
 onready var _section_name = $Pivot/SectionName
-onready var _icon_row_container = $Pivot/SectionName/IconRows
+onready var _icon_containers = $Pivot/SectionName/Icons
 
 export(Resource) var wheel_section_data setget _set_data
 
@@ -26,11 +26,12 @@ func _set_data(section_data):
 	wheel_section_data = section_data
 	if wheel_section_data:
 		wheel_section_data.connect("changed", self, "_update_wheel")
-		_update_wheel()
+		if is_inside_tree():
+			_update_wheel()
 
 
 func _update_wheel():
-	if not wheel_section_data:
+	if not wheel_section_data or not is_inside_tree():
 		return
 
 	_update_name()
@@ -43,9 +44,6 @@ func _update_wheel():
 #     NAME
 # =============
 func _update_name():
-	if not has_node("Pivot/SectionName"):
-		return
-
 	if not _section_name:
 		_section_name = $Pivot/SectionName
 	_section_name.text = wheel_section_data.section_name
@@ -55,33 +53,41 @@ func _update_name():
 #     ICONS
 # =============
 func _update_icons():
-	if not has_node("Pivot/SectionName/IconRows"):
-		return
+	if not _icon_containers:
+		_icon_containers = $Pivot/SectionName/Icons
 
-	if not _icon_row_container:
-		_icon_row_container = $Pivot/SectionName/IconRows
-
-	for child in _icon_row_container.get_children():
+	for child in _icon_containers.get_children():
 		child.queue_free()
 
 	if wheel_section_data.miss:
-		_add_icon_row(1, miss_icon, "Miss")
+		_add_icon_grid(1, miss_icon, "Miss")
 	else:
-		_add_icon_row(wheel_section_data.heal_points, heart_icon, "Heal")
-		_add_icon_row(wheel_section_data.attack_points, sword_icon, "Attack")
-		_add_icon_row(wheel_section_data.special_points, lightning_icon, "Special")
-		_add_icon_row(wheel_section_data.defense_points, shield_icon, "Defense")
+		_add_icon_grid(wheel_section_data.heal_points, heart_icon, "Heal")
+		_add_icon_grid(wheel_section_data.attack_points, sword_icon, "Attack")
+		_add_icon_grid(wheel_section_data.special_points, lightning_icon, "Special")
+		_add_icon_grid(wheel_section_data.defense_points, shield_icon, "Defense")
 
 
-func _add_icon_row(amount, texture, name):
+func _add_icon_grid(amount, texture, name):
 	if amount <= 0:
 		return
-	var icon_row := HBoxContainer.new()
-	icon_row.name = name
-	icon_row.alignment = BoxContainer.ALIGN_CENTER
-	_icon_row_container.add_child(icon_row)
-	icon_row.owner = get_tree().edited_scene_root
-	_add_icons(icon_row, amount, texture, name)
+
+	var max_columns = 4
+	var icon_grid := GridContainer.new()
+	_icon_containers.add_child(icon_grid)
+	icon_grid.name = name
+	icon_grid.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+
+	if amount < max_columns:
+		icon_grid.columns = amount
+	else:
+		if amount / 2 < max_columns:
+			icon_grid.columns = amount / 2
+		else:
+			icon_grid.columns = max_columns
+
+	icon_grid.owner = get_tree().edited_scene_root
+	_add_icons(icon_grid, amount, texture, name)
 
 
 func _add_icons(container, amount, texture, node_prefix = "TexRect"):
@@ -89,10 +95,9 @@ func _add_icons(container, amount, texture, node_prefix = "TexRect"):
 		var tex_rect := TextureRect.new()
 		tex_rect.texture = texture
 		tex_rect.name = node_prefix + "_" + str(i + 1)
-		tex_rect.stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
+		tex_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 		container.add_child(tex_rect)
 		tex_rect.owner = get_tree().edited_scene_root
-		tex_rect.rect_scale = Vector2(2, 2)
 
 
 # =============
@@ -106,11 +111,13 @@ func _update_visibility():
 #  POSITIONING
 # =============
 func _update_positioning():
-	if not has_node("Pivot"):
-		return
-
 	if not _pivot:
 		_pivot = $Pivot
 
-	var default_rotation = PI # 180 deg
+	var default_rotation = PI  # 180 deg
 	_pivot.set_rotation(default_rotation * wheel_section_data.percent_of_wheel)
+
+	if wheel_section_data.section_name.empty():
+		_icon_containers.rect_position.y = 0
+	else:
+		_icon_containers.rect_position.y = _section_name.rect_size.y
