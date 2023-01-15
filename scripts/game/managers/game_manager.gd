f6d94178c927e494e80dac6a137e10cf212e8901
Trevor <tkhowell95@gmail.com>
2023-01-15 15:14:58 -0700

    Full quest completion

--- scripts/game/managers/game_manager.gd
+++ scripts/game/managers/game_manager.gd
@@ -10,9 +10,16 @@ var _overlord_quest: Quest
 
 func set_hero_quest(quest: Quest):
 	_hero_quest = quest
+	Utils.connect_signal(_hero_quest, "completed", self, "_end_game", [true], CONNECT_ONESHOT)
 	_hero_quest.begin_tracking()
 
 
 func set_overlord_quest(quest: Quest):
 	_overlord_quest = quest
+	Utils.connect_signal(_overlord_quest, "completed", self, "_end_game", [false], CONNECT_ONESHOT)
 	_overlord_quest.begin_tracking()
+
+
+func _end_game(quest, was_hero_quest: bool):
+	var who = "hero" if was_hero_quest else "overlord"
+	print("Game over, " + who + " player wins!")
