3efa4d67c92d1711e8d931380ec1d1a8cf0a09e3
Trevor <tkhowell95@gmail.com>
2023-01-29 12:13:05 -0700

    Created equipment & weapon data resource scripts

---
+++ scripts/data/equipment_data.gd
@@ -0,0 +1,24 @@
+extends Resource
+
+class_name Equipment
+
+enum EquipSlots { head, torso, foot, hand }
+var HEAD_SLOTS = 1
+var TORSO_SLOTS = 1
+var FOOT_SLOTS = 1
+var HAND_SLOTS = 2
+
+# Which slot does this equipment belong to
+export(EquipSlots) var slot
+
+# How many slots does this equipment take up
+export(int) var slot_count := 1
+
+# Arbitrary strings describing the object (e.g. cloak, rune, dagger, etc)
+export(PoolStringArray) var attributes
+
+# Any actions the equipment provides in addition to base hero actions
+export(Array) var extra_actions
+
+# Any actions the equipment provides available with surge points
+export(Array) var surge_actions
---
+++ scripts/data/weapon_data.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name WeaponData
+
+# Wheel sections to use during attacks
+export(Array) var wheel_sections
+
+# Equipment data for this weapon
+export(Resource) var equipment_data
+
+# Is the weapon a ranged weapon
+export(bool) var is_ranged
