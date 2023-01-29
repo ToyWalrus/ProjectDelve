9656b3b43c1d146f27fe2233135362c510b1e388
Trevor <tkhowell95@gmail.com>
2023-01-29 12:30:48 -0700

    Created a WeaponData object

--- scripts/data/equipment_data.gd
+++ scripts/data/equipment_data.gd
@@ -1,12 +1,15 @@
 extends Resource
 
-class_name Equipment
+class_name EquipmentData
 
 enum EquipSlots { head, torso, foot, hand }
 var HEAD_SLOTS = 1
 var TORSO_SLOTS = 1
 var FOOT_SLOTS = 1
 var HAND_SLOTS = 2
+
+# The name of this equipment
+export(String) var name
 
 # Which slot does this equipment belong to
 export(EquipSlots) var slot
