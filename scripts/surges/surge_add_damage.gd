4642a2d7bab7880e5b831a49948d5c6c6710eaef
Trevor <tkhowell95@gmail.com>
2023-01-31 18:48:29 -0700

    Updated the weapons with some surges

--- scripts/surges/surge_add_damage.gd
+++ scripts/surges/surge_add_damage.gd
@@ -1,5 +1,7 @@
 extends Resource
 
+class_name SurgeAddDamage
+
 export(int) var extra_damage := 1
 
 
@@ -7,4 +9,5 @@ func apply_to_wheel(result: WheelSectionData):
 	if result.special_points > 0:
 		result.special_points -= 1
 		result.attack_points += extra_damage
+		print("Added " + str(extra_damage) + " extra damage from surge!")
 	return result
--- scripts/surges/surge_add_range.gd
+++ scripts/surges/surge_add_range.gd
@@ -1,5 +1,7 @@
 extends Resource
 
+class_name SurgeAddRange
+
 export(int) var extra_range := 1
 
 
@@ -7,4 +9,5 @@ func apply_to_wheel(result: WheelSectionData):
 	if result.special_points > 0:
 		result.special_points -= 1
 		result.attack_points += extra_range
+		print("Added " + str(extra_range) + " extra range from surge!")
 	return result
